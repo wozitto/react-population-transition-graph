50c4642a934a73b02a5010099cec025be8b82726
wozitto <hirookatoshito@gmail.com>
2022-04-23 10:52:10 +0900

    prettier eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+        'semi-spacing': ['error', { after: true, before: false }],
+        'semi-style': ['error', 'first'],
+        'no-extra-semi': 'error',
+        'no-unexpected-multiline': 'error',
+        'no-unreachable': 'error',
+        "react/react-in-jsx-scope": "off"
+    }
+}
